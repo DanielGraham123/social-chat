aa9238c03125a0785f955a7f8afd144b5e896a78
DanielGraham123 <dg.danielboaz@yahoo.com>
2023-07-24 13:37:11 +0100

    no origin header

--- socket/index.ts
+++ socket/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: "*;*",
     methods: ["GET", "POST"],
   },
   allowRequest: (req, callback) => {
